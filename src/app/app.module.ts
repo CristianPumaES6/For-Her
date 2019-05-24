c8ad82a15243009731e77bdec84874899ca1558a
CristianPumaES6 <cpuma@inconcertcc.com>
2019-05-23 22:25:18 -0500

    Primeros commit, se estubo pasando el template de boostrap a una infraestructura de angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
